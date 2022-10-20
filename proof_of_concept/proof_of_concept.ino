4b59531f43b3ac4be6f745ea6141486a8d5651ce
Jonas <stlljonas@gmail.com>
2022-10-20 15:08:41 +0200

    Reduced sketch to minimum setup to display some text

--- proof_of_concept/proof_of_concept.ino
+++ proof_of_concept/proof_of_concept.ino
@@ -1,7 +1,4 @@
 #include <lvgl.h>
-//#include <lv_examples.h>
-//#include <lv_demos.h>
-#include <demos/lv_demos.h>
 #include <TFT_eSPI.h>
 
 // following https://github.com/lvgl/lvgl/blob/master/examples/arduino/LVGL_Arduino/LVGL_Arduino.ino
@@ -10,15 +7,10 @@
 #define N_PX_H 240 // height
 
 static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[ N_PX_W * 10]; // this can be smaller, idk the minimal number for it to work
-static lv_obj_t * tab_view;
+static lv_color_t buf[ N_PX_W * 10]; // this can be smaller, the minimal number for it to work is unknown
 
 TFT_eSPI tft =
     TFT_eSPI(N_PX_W, N_PX_H); // Invoke library, pins defined in User_Setup.h
-
-unsigned long tab_switch_time_ms = 1000;
-unsigned long last_tab_switch_timestamp = 0;
-int tab_idx = 0;
 
 /* Display flushing */
 void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p ) {
@@ -31,7 +23,7 @@ void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *colo
     tft.pushColors( ( uint16_t * )&color_p->full, w * h, true ); // whats this color_p->full?
     tft.endWrite();
 
-    lv_disp_flush_ready( disp ); // ??
+    lv_disp_flush_ready( disp );
 }
 
 void setup() {
@@ -40,38 +32,22 @@ void setup() {
     tft.setRotation(1); // on change, hor_res and ver_res of disp_drv might need to be switched
     lv_disp_draw_buf_init( &draw_buf, buf, NULL, N_PX_W * 10 );
 
-    /*Initialize the display*/
+    /* Initialize the display */
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init( &disp_drv );
-    /*Change the following line to your display resolution*/
+    /* Change the following lines to your display resolution */
     disp_drv.hor_res = N_PX_H;
     disp_drv.ver_res = N_PX_W;
     disp_drv.flush_cb = my_disp_flush;
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register( &disp_drv );
-
-    lv_coord_t tab_height = N_PX_W/6; // h = height !
-
-    // Create tabview
-    tab_view = lv_tabview_create(lv_scr_act(), LV_DIR_TOP, tab_height);
-    lv_obj_t * t1 = lv_tabview_add_tab(tab_view, "Tab1");
-    lv_obj_t * t2 = lv_tabview_add_tab(tab_view, "Tab2");
-    lv_obj_t * t3 = lv_tabview_add_tab(tab_view, "Tab3");
-
-    // Add content to tab in from of label
-    lv_obj_t * content1 = lv_label_create(t1);
-    lv_obj_t * content2 = lv_label_create(t2);
-    lv_obj_t * content3 = lv_label_create(t3);
-    lv_label_set_text(content1, "Tab1 content");
-    lv_label_set_text(content2, "Tab2 content");
-    lv_label_set_text(content3, "Tab3 content");
+    
+    // display some text
+    lv_obj_t * text = lv_label_create(lv_scr_act());
+    lv_label_set_text(text, "Hello World");
 }
+
 void loop() { // loop looks good
-    if (millis() - last_tab_switch_timestamp >= tab_switch_time_ms) {
-        tab_idx = (tab_idx+1)%3;
-        lv_tabview_set_act(tab_view, tab_idx, LV_ANIM_ON);
-        last_tab_switch_timestamp = millis();
-    }
     lv_timer_handler(); /* let the GUI do its work */
     delay(5);
 }
