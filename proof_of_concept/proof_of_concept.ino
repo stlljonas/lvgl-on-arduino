e7964e879ae5b1c0c30095cb5a6385207deae7b8
Jonas <stlljonas@gmail.com>
2022-10-27 18:12:22 +0200

    Add Sensi home screen and functionize widget creation

--- proof_of_concept/proof_of_concept.ino
+++ proof_of_concept/proof_of_concept.ino
@@ -1,5 +1,6 @@
 #include <lvgl.h>
 #include <TFT_eSPI.h>
+// #include "sensi_logo_small.c"
 
 // following https://github.com/lvgl/lvgl/blob/master/examples/arduino/LVGL_Arduino/LVGL_Arduino.ino
 
@@ -8,14 +9,48 @@
 
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ N_PX_W * 10]; // this can be smaller, idk the minimal number for it to work
-static lv_obj_t * tab_view;
+
+static lv_obj_t * screen = NULL;
 
 TFT_eSPI tft =
     TFT_eSPI(N_PX_W, N_PX_H); // Invoke library, pins defined in User_Setup.h
 
-unsigned long tab_switch_time_ms = 1000;
-unsigned long last_tab_switch_timestamp = 0;
-int tab_idx = 0;
+static lv_style_t * app_style = NULL;
+
+static float value = 0;
+
+LV_IMG_DECLARE(sensi_logo_small); // required if online converter was used
+
+lv_obj_t * create_widget(const char* quantity, const char* value, const char* unit, const char* device_name, lv_obj_t * parent, lv_color_t highlight_color = lv_color_white()) {
+    lv_obj_t * widg = lv_obj_create(parent);
+    
+    lv_obj_set_size(widg, 220,110);
+    lv_obj_set_align(widg, LV_ALIGN_CENTER);
+    lv_obj_add_style(widg, app_style, 0);
+    lv_obj_set_style_border_color(widg, highlight_color, 0);
+
+    lv_obj_t * value_label = lv_label_create(widg);
+    lv_label_set_text(value_label, value);
+    lv_obj_set_align(value_label, LV_ALIGN_CENTER);
+    lv_obj_set_style_text_color(value_label, highlight_color, 0);
+    lv_obj_set_style_text_font(value_label, &lv_font_montserrat_30, LV_STATE_DEFAULT);
+    // maybe use the states to change the colors? (red, orange, green)
+
+    lv_obj_t * quantity_label = lv_label_create(widg);
+    lv_label_set_text(quantity_label, quantity);
+    lv_obj_align_to(quantity_label, value_label, LV_ALIGN_OUT_LEFT_MID, -15, 4);
+
+
+    lv_obj_t * unit_label = lv_label_create(widg);
+    lv_label_set_text(unit_label, unit);
+    lv_obj_align_to(unit_label, value_label, LV_ALIGN_OUT_RIGHT_MID, 10 , 4);
+
+    lv_obj_t * device_name_label = lv_label_create(widg);
+    lv_label_set_text(device_name_label, device_name);
+    lv_obj_set_align(device_name_label, LV_ALIGN_BOTTOM_MID);    
+    
+    return widg;
+}
 
 /* Display flushing */
 void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p ) {
@@ -33,7 +68,6 @@ void setup() {
     tft.begin();
     tft.setRotation(1); // on change, hor_res and ver_res of disp_drv might need to be switched
     lv_disp_draw_buf_init( &draw_buf, buf, NULL, N_PX_W * 10 );
-
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init( &disp_drv );
     // Change the following line to your display resolution
@@ -43,28 +77,82 @@ void setup() {
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register( &disp_drv );
 
-    lv_coord_t tab_height = N_PX_W/6; // h = height !
+    screen = lv_scr_act();
 
-    // Create tabview
-    tab_view = lv_tabview_create(lv_scr_act(), LV_DIR_TOP, tab_height);
-    lv_obj_t * t1 = lv_tabview_add_tab(tab_view, "Tab1");
-    lv_obj_t * t2 = lv_tabview_add_tab(tab_view, "Tab2");
-    lv_obj_t * t3 = lv_tabview_add_tab(tab_view, "Tab3");
+    // Create dark style
 
-    // Add content to tab in from of label
-    lv_obj_t * content1 = lv_label_create(t1);
-    lv_obj_t * content2 = lv_label_create(t2);
-    lv_obj_t * content3 = lv_label_create(t3);
-    lv_label_set_text(content1, "Tab1 content");
-    lv_label_set_text(content2, "Tab2 content");
-    lv_label_set_text(content3, "Tab3 content");
+    static lv_style_t dark_style;
+
+    lv_style_init(&dark_style);
+    lv_style_set_bg_color(&dark_style, lv_color_black());
+    lv_style_set_text_color(&dark_style, lv_color_white());
+    app_style = &dark_style; // apply dark style
+
+    lv_obj_add_style(screen, app_style, LV_STATE_DEFAULT);
+    // lv_style_set_prop_meta(&dark_style, LV_STYLE_PROP_ANY, LV_STYLE_PROP_META_INITIAL);
+    // */
+    // static lv_style_t dark_style_big_font = dark_style;
+    // lv_style_set_text_font(&dark_style_big_font, LV_FONT_MONTSERRAT_24);
+
+    lv_obj_t * logo = lv_img_create(screen);
+    lv_img_set_src(logo, &sensi_logo_small);
+    lv_obj_set_align(logo, LV_ALIGN_CENTER);
+    lv_timer_handler();
+    delay(2000);
+    lv_obj_add_flag(logo, LV_OBJ_FLAG_HIDDEN);
+    // lv_disp_t
+    // Create dark sensi theme
+
+    // lv_theme_t * th = lv_theme_default_init(,  /*Use the DPI, size, etc from this display*/ 
+    //                                         LV_COLOR_PALETTE_BLUE, LV_COLOR_PALETTE_CYAN,   /*Primary and secondary palette*/
+    //                                         false,    /*Light or dark mode*/ 
+    //                                         &lv_font_montserrat_10, &lv_font_montserrat_14, &lv_font_montserrat_18); /*Small, normal, large fonts*/
+                                            
+    // lv_disp_set_theme(display, th); /*Assign the theme to the display*/
+    // lv_theme_apply(screen);
+
+     /*lv_obj_create(screen);
+    lv_obj_set_size(widg, 220,110);
+    lv_obj_set_align(widg, LV_ALIGN_CENTER);
+    lv_obj_add_style(widg, app_style, 0);
+    lv_obj_set_style_border_color(widg, highlight_color, 0);
+
+    lv_obj_t * value_label = lv_label_create(widg);
+    lv_label_set_text(value_label, buffer);
+    lv_obj_set_align(value_label, LV_ALIGN_CENTER);
+    lv_obj_set_style_text_color(value_label, highlight_color, 0);
+    lv_obj_set_style_text_font(value_label, &lv_font_montserrat_30, LV_STATE_DEFAULT);
+    // maybe use the states to change the colors? (red, orange, green)
+
+    lv_obj_t * quantity_label = lv_label_create(widg);
+    lv_label_set_text(quantity_label, quantity);
+    lv_obj_align_to(quantity_label, value_label, LV_ALIGN_OUT_LEFT_MID, -15, 4);
+
+
+    lv_obj_t * unit_label = lv_label_create(widg);
+    lv_label_set_text(unit_label, unit);
+    lv_obj_align_to(unit_label, value_label, LV_ALIGN_OUT_RIGHT_MID, 10 , 4);
+
+    lv_obj_t * device_name_label = lv_label_create(widg);
+    lv_label_set_text(device_name_label, device_name);
+    lv_obj_set_align(device_name_label, LV_ALIGN_BOTTOM_MID);*/
 }
+
 void loop() {
-    if (millis() - last_tab_switch_timestamp >= tab_switch_time_ms) {
-        tab_idx = (tab_idx+1)%3;
-        lv_tabview_set_act(tab_view, tab_idx, LV_ANIM_ON);
-        last_tab_switch_timestamp = millis();
-    }
+    // lv_label_set_text(value_label, buffer);
+
+    char* quantity = "T:";
+    char* unit = "degC";
+    char* device_name = "gadgetID";
+
+    char buffer[6];
+    int ret = snprintf(buffer, sizeof buffer, "%01f", value);    
+    // Build a sensor reading widget
+    lv_obj_clean(screen); // implicit deletion of dynamically allocated widget
+    // lv_color_t highlight_color = lv_color_hex(0xFF0000); // red
+    lv_color_t highlight_color = lv_color_hex(0x00FF00); // green
+    lv_obj_t * widg = create_widget(quantity, buffer, unit, device_name, screen, highlight_color);
     lv_timer_handler(); /* let the GUI do its work */
     delay(5);
+    value += 1;
 }
